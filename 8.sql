0731cb0ba17275fe63424ece347582850db1db86
syma <59690768+symaysun@users.noreply.github.com>
2021-11-02 20:35:16 +0300

    Create 8.sql
    1-test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
    2-Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
    3-Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
    4-Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.

---
+++ 8.sql
@@ -0,0 +1,29 @@
+CREATE TABLE employee (ıd serial primary key , 
+				 name VARCHAR(50), 
+				 birthday DATE, 
+				 email VARCHAR(100));
+         
+         
+         
+         2. SORU YAPILDI
+         
+         
+         
+         
+         
+UPDATE employee
+SET name = 'Şeyma ',
+    email='seymaysun@gmail.com',
+    birthday='13-05-2011'     Toplamda üç adet için değiştirdik.
+    
+    
+    
+    
+DELETE
+FROM employee
+WHERE birthday >'13-05-2007'     
+    
+    
+    
+    
+    
